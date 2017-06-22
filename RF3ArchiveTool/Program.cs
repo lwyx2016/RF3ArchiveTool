bba07bf2ca3b6a20951ef905c6060ef94c9914dd
lwyx2016 <710010028@qq.com>
2017-06-22 22:55:53 +0800

    Create Program.cs

---
+++ RF3ArchiveTool/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RF3ArchiveTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("RF3ArchiveTool");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Drag and drop the rf3Archive.arc on this program");
+            Console.WriteLine("The project is not completed!!!");
+            Console.ResetColor();
+        }
+        
+    }
+}

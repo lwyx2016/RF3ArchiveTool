874f3c949a0c33b5ebaf60f4ba48c63990cfaaac
lwyx2016 <710010028@qq.com>
2017-06-27 23:35:57 +0800

    Add files via upload

---
+++ FileRenamer/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Collections.ObjectModel
+
+Class MainWindow
+
+    Private Sub TextBlock_Drop(sender As Object, e As DragEventArgs)
+        Dim f = CType(e.Data.GetData(DataFormats.FileDrop), String())
+        RenameFiles(f)
+    End Sub
+    Private Sub RenameFiles(f As String())
+        Dim ren As New FileRenamer
+        System.Threading.Tasks.Parallel.ForEach(f, Sub(fn As String)
+                                                       Try
+                                                           ren.auto_rename(fn)
+                                                       Catch ex As Exception
+                                                       End Try
+                                                   End Sub)
+    End Sub
+End Class
